e9fb627b83131277107454f86509d3c8ccf2f789
Vikas Arora <vikasarora200@gmail.com>
2025-06-30 02:04:08 +0530

    Initial Commit ~2hr timeline

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7;
+    [SerializeField] private GameInput gameInput;
+    private bool isWalking;
+    private void Update()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        isWalking = moveDir != Vector3.zero;
+        
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+}
