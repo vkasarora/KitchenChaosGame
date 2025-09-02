c73ae6bead1ec39aec5bfe106313f51e7d70e386
Vikas Arora <vikasarora200@gmail.com>
2025-09-03 00:14:52 +0530

    Improve player movement logic to ensure movement only occurs when a direction is specified

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -98,7 +98,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
         {
             // try to move in x direction
             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
-            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            canMove = moveDir.x != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
 
             if (canMove)
             {
@@ -108,7 +108,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
             {
                 // Try moving in z direction
                 Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
-                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                canMove = moveDir.z != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
 
                 if (canMove)
                 {
